fd73b971b63a056701f9abbb21e1696bbf2e5fb4
Ajuuke <asarajoog@gmail.com>
2024-11-21 20:43:59 -0500

    Start to add becoming tutor logic

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
